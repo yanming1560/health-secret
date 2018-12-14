eed3edadd278861bf0a598f94f04216fcffc7104
yanming1560 <36906575+yanming1560@users.noreply.github.com>
2018-12-14 16:36:24 +0800

    Update food_information.py

--- food/food_information.py
+++ food/food_information.py
@@ -1,2 +1,22 @@
-https://www.boohee.com
 
+import requests,random,time
+from bs4 import BeautifulSoup as bs
+
+url_food="https://www.boohee.com/food/group/1?page=1"       #薄荷网
+url_menu="https://www.boohee.com/food/view_menu?page=1"
+url="https://yingyang.51240.com/xiaomai__yingyangshow/"       #便民查询网
+
+
+allhead=['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+         'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36 OPR/26.0.1656.60',
+         'Opera/8.0 (Windows NT 5.1; U; en)',
+        'Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50',
+        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 9.50',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
+        'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
+         'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko']
+
+aa = requests.get(url, headers={'user-agent': random.choice(allhead)})
+bb = bs(aa.content, 'lxml')
+print(bb)
